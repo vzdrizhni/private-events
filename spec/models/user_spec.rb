9caab37d609d7a25e52ddddb951064c8054ee289
Alexandru <rammazzoti2000@gmail.com>
2020-05-01 16:04:33 +0200

    rspec validation & association User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "associations" do
+    it { should have_many(:events).with_foreign_key('creator_id') }
+    it { should have_many(:invitations).with_foreign_key('attendee_id') }
+    it { should have_many(:attended_events).through(:invitations).source(:attended_event) }
+  end
+  
+  describe "validations" do
+    subject { User.create(name:"nam", email:"example@mail.com") }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(4).is_at_most(20) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('example@email.com').for(:email) }
+  end
 end
