56217462ec599db8c8ba2d3bc100ca0d1ba66178
Alexandru <rammazzoti2000@gmail.com>
2020-05-02 19:46:58 +0200

    Capybara authentication for login process

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -25,3 +25,31 @@ feature 'the signup process' do
     end
   end
 end
+
+feature 'the signin process' do
+
+  scenario 'has a login user page' do
+    visit(login_url)
+    expect(page).to have_content('Email')
+  end
+
+  feature 'signing in a user' do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Name', with: 'testing'
+      fill_in 'Email', with: 'biscuits@gmail.com'
+      click_on 'Create User'
+      visit login_url
+      fill_in 'Email', with: 'biscuits@gmail.com'
+      click_button 'Log in'
+    end
+
+    scenario 'redirects to home page after signin' do
+      expect(page).to have_content 'User: '
+    end
+
+    scenario 'shows username on the homepage after signing in' do
+      expect(page).to have_content 'testing'
+    end
+  end
+end
