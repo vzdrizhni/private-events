47bc79e78f2749b77b978d67d8b7278eaf742d76
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-05-08 00:47:07 +0200

    when new user created it signs in automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to root_path, notice: "User was successfully created. #{@user.name} please login." }
+        user = User.find_by(user_params)
+        session[:user_id] = user.id
+        format.html { redirect_to root_path, notice: "User was successfully created. Welcome #{@user.name}." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
